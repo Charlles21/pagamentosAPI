93f93e376a9cdc918278c4f12a71184d427e4922
Charlles <matheuscharles453@gmail.com>
2022-07-08 14:58:08 -0300

    master

--- src/main/java/br/com/alurafood/pagamentos/Repository/PagamentoRepository.java
+++ src/main/java/br/com/alurafood/pagamentos/Repository/PagamentoRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.alurafood.pagamentos.Model.Pagamento;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long>{
+
+
+
+    
     
 }
