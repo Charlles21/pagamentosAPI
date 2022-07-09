5b0986c77cda192392b7c558463779fb7bcccf07
Charlles <matheuscharles453@gmail.com>
2022-07-09 16:39:52 -0300

    master

--- src/main/java/br/com/alurafood/pagamentos/Repository/PagamentoRepository.java
+++ src/main/java/br/com/alurafood/pagamentos/Repository/PagamentoRepository.java
@@ -1,10 +1,15 @@
 package br.com.alurafood.pagamentos.Repository;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.alurafood.pagamentos.Dto.PagamentoDto;
 import br.com.alurafood.pagamentos.Model.Pagamento;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long>{
+
+    Page<PagamentoDto> findAll(Pageable paginacao);
 
 
 
